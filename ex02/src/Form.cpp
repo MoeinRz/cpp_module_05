6a737c0a894f54c27e1be5765800e858c24039ea
MoeinRz <moeinrezaei330@gmail.com>
2024-01-17 23:54:27 +0100

    ex02 is updated.

---
+++ ex02/src/Form.cpp
@@ -0,0 +1,75 @@
+#include "../include/Form.hpp"
+
+Form::Form() : _name("Default"), _isSigned(false), _gradeToSign(150), _exeGrade(150)
+{
+    std::cout << "Form " << this->_name << " created." << std::endl;
+    return;
+}
+
+Form::Form(const std::string name, bool indicate, int signGrade, int exeGrade) :
+_name(name), _isSigned(indicate), _gradeToSign(signGrade), _exeGrade(exeGrade)
+{
+    if (_gradeToSign < 1 || _exeGrade < 1)
+    {
+		throw GradeTooHighException();
+    }
+    else if (_gradeToSign > 150 || _exeGrade > 150)
+    {
+        throw GradeTooLowException();
+    }
+    std::cout << "Form " << this->_name << " created." << std::endl;
+    return;
+}
+
+Form::~Form()
+{
+    std::cout << "Form " << this->_name << " destrucred." << std::endl;
+    return; 
+}
+
+std::string Form::getName() const
+{
+    return this->_name;
+}
+
+bool Form::getIndicate() const
+{
+    return this->_isSigned;
+}
+
+int Form::getSignGrade() const
+{
+    return this->_gradeToSign;
+}
+
+int Form::getExeGrade() const
+{
+    return this->_exeGrade;
+}
+
+std::ostream& operator<<(std::ostream& out, const Form& src)
+{
+    out << src.getName() << ", Form grade ";
+    if (src.getIndicate() == false)
+        out << "not ";
+    out << "signed.\nsignGrade is: " << src.getSignGrade() << "\nexeGrade is: " << src.getExeGrade() << std::endl;
+    return out;
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high.";
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low.";
+}
+
+void Form::beSigned(int bureaucratGrade)
+{
+    if (bureaucratGrade <= _gradeToSign)
+        _isSigned = true;
+    else
+        throw Form::GradeTooLowException();
+}
---
+++ ex02/src/main.cpp
@@ -0,0 +1,67 @@
+#include "../include/Bureaucrat.hpp"
+#include "../include/Form.hpp"
+
+void leake()
+{
+    system("leaks a.out");
+}
+
+int main()
+{
+    std::cout << "\n[0]=====================================\n" << std::endl;
+    try
+    {
+        Bureaucrat moein("Moein", 80);
+        Form form("CPP_Module_05", false, 80, 50);
+
+        std::cout << "Original Form: " << form << std::endl;
+        moein.signForm(form);
+        std::cout << "Updated Form: " << form << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+
+    std::cout << "\n[1]=====================================\n" << std::endl;
+    try
+    {
+        Bureaucrat moein("Moein", 80);
+        Form form("CPP_Module_05", false, 10, 50);
+
+        std::cout << "Original Form: " << form << std::endl;
+        moein.signForm(form);
+        std::cout << "Updated Form: " << form << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+
+    std::cout << "\n[2]=====================================\n" << std::endl;
+    try
+    {
+        Form fafa("fafa", false, 0, 1);
+    }
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+    
+    std::cout << "\n[3]=====================================\n" << std::endl;
+    try
+    {
+        Form fafa("fafa", false, 2, 151);
+    }
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+    std::cout << "\n[END]=====================================" << std::endl;
+
+// leake();
+return 0;
+}
